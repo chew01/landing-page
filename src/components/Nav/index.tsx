ce5b13d60f6d1205a6275aa9ad5dfbfbd4778bdb
chew01 <chewjh19@gmail.com>
2022-04-08 11:55:07 +0800

    Added resume link

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  FaGithub, FaLinkedin, FaTwitter, FaBook, FaJenkins,
+  FaGithub, FaLinkedin, FaTwitter, FaBook, FaJenkins, FaFilePdf,
 } from 'react-icons/fa';
 import FadeIn from 'react-fade-in';
 import Link from '../Link';
@@ -17,9 +17,10 @@ function Nav(props: NavProps) {
       wrapperTag={'nav' as any}
       visible={titleOver}
     >
+      <Link title="Portfolio" href="https://chew01.github.io/" icon={FaBook} />
+      <Link title="Resume" href="https://fs.kitsuiro.com/docs/CV_Daniel.pdf" icon={FaFilePdf} />
       <Link title="Github" href="https://github.com/chew01" icon={FaGithub} />
       <Link title="LinkedIn" href="https://www.linkedin.com/in/chewjunheng/" icon={FaLinkedin} />
-      <Link title="Portfolio" href="https://chew01.github.io/" icon={FaBook} />
       <Link title="Twitter" href="https://twitter.com/lifelessfruit" icon={FaTwitter} />
       <Link title="Jenkins" href="https://jenkins.kitsuiro.com/" icon={FaJenkins} />
     </FadeIn>
